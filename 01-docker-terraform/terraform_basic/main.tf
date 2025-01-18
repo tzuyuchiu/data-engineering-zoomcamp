04445d7b8705d6d94180d5b827d2658ad887a98d
Tzu Yu Chiu <tzuyujulia@gmail.com>
2025-01-18 23:01:19 +0100

    add terraform basic files

---
+++ 01-docker-terraform/terraform_basic/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.16.0"
+    }
+  }
+}
+
+provider "google" {
+    project     = "festive-music-448221-u0"
+    region      = "europe-west1-b"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "festive-music-448221-u0-terraform-bucket"
+  location      = "EUROPE-WEST1"
+
+  # Optional, but recommended settings:
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  }
+
+  force_destroy = true
+}
+
+
