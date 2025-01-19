0862890a5b2ef44b9ea356dac207a04d3a4ebbf6
Tzu Yu Chiu <tzuyujulia@gmail.com>
2025-01-19 21:24:35 +0100

    add docker postgres files

--- 01-docker-terraform/terraform_basic/main.tf
+++ 01-docker-terraform/terraform_basic/main.tf
@@ -1,27 +1,27 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "6.16.0"
     }
   }
 }
 
 provider "google" {
-    project     = "festive-music-448221-u0"
-    region      = "europe-west1-b"
+  project = "festive-music-448221-u0"
+  region  = "europe-west1-b"
 }
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "festive-music-448221-u0-terraform-bucket"
-  location      = "EUROPE-WEST1"
+  name     = "festive-music-448221-u0-terraform-bucket"
+  location = "EUROPE-WEST1"
 
   # Optional, but recommended settings:
-  storage_class = "STANDARD"
+  storage_class               = "STANDARD"
   uniform_bucket_level_access = true
 
   versioning {
-    enabled     = true
+    enabled = true
   }
 
   lifecycle_rule {
@@ -29,11 +29,14 @@ resource "google_storage_bucket" "demo-bucket" {
       type = "Delete"
     }
     condition {
-      age = 30  // days
+      age = 30 // days
     }
   }
 
   force_destroy = true
 }
 
+resource "google_bigquery_dataset" "demo-dataset" {
+  dataset_id = "demo_dataset"
 
+}
